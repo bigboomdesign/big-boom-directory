7d8632eabff79cf9bcc16461c3eaa8d66ffbf091
Michael Hull <michael@resoundingechoes.net>
2015-09-10 20:20:21 -0400

    updated version

--- cpt-directory.php
+++ cpt-directory.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Custom Post Type Directory
  * Description: Creates a directory based on Custom Post Type, Taxonomy, and Fields
- * Version: 1.8.0
+ * Version: 1.9.0
  * Author: Big Boom Design
  * Author URI: http://bigboomdesign.com
  */
