a1dc37141d6131ab65caafddbc2cf8a6acc0c007
Michael Hull <michael@resoundingechoes.net>
2015-12-14 13:21:15 -0500

    Updated the version number

--- cpt-directory.php
+++ cpt-directory.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Custom Post Type Directory
  * Description: Directory management system based on Custom Post Types, Taxonomies, and Fields
- * Version: 2.0.0.28.1
+ * Version: 2.0.0.28.2
  * Author: Big Boom Design
  * Author URI: http://bigboomdesign.com
  */
