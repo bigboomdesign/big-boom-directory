e34377970ecebd98c3d17667e3d112430234eca2
Michael Hull <michael@resoundingechoes.net>
2017-01-23 18:58:30 -0500

    Make sure the text domain matches the plugin folder name

--- big-boom-directory.php
+++ big-boom-directory.php
@@ -7,7 +7,7 @@
  * Author URI: https://bigboomdesign.com
  * License:     GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: bbd
+ * Text Domain: big-boom-directory
  */
  
 /**
